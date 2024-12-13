cce14bd02b878593d75ebdc10f80d1976895288b
Kratik Paliwal <kratikpaliwal@Kratiks-MacBook-Air.local>
2024-12-13 16:42:44 +0530

    Done with it

---
+++ index.js
@@ -0,0 +1,16 @@
+let container = document.querySelector('#main-container')
+let heart = document.querySelector('i')
+container.addEventListener('dblclick',()=>{
+    heart.style.transform='translate(-50%, -50%) scale(1)'
+    heart.style.opacity=0.8;
+    
+    setTimeout(() => {
+        heart.style.opacity=0;
+    }, 1000); 
+    setTimeout(()=>{
+        heart.style.transform='translate(-50%, -50%) scale(0)'
+    },1000)
+    
+
+
+})
